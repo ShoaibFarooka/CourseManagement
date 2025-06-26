06df70d0de39a80afd8bc479ad02d6f8de381a77
Shoaib Farooka <shoaibfarooka@gmail.com>
2025-06-26 18:30:35 +0500

    course model added

---
+++ server/models/courseModel.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+
+const subunitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+});
+
+const unitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    type: {
+        type: String,
+        enum: ['rapid', 'mcq', 'essay'],
+        required: true,
+    },
+    subunits: {
+        type: [subunitSchema],
+        required: true
+    },
+});
+
+const partSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    units: {
+        type: [unitSchema],
+        required: true
+    },
+});
+
+const publisherSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+});
+
+const courseSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        publishers: {
+            type: [publisherSchema],
+            required: true
+        },
+        parts: {
+            type: [partSchema],
+            required: true
+        },
+        status: {
+            type: Boolean,
+            default: true
+        }
+    },
+    { timestamps: true }
+);
+
+const Course = mongoose.model("course", courseSchema);
+
+module.exports = Course;
