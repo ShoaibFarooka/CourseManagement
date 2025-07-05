995e1c5a3501f9537409bb0a72cf023c74ce26a2
Shoaib Farooka <shoaibfarooka@gmail.com>
2025-07-06 01:15:35 +0500

    select state clear fixed

--- client/src/components/Select/SelectDropDown.jsx
+++ client/src/components/Select/SelectDropDown.jsx
@@ -1,12 +1,13 @@
 import './SelectDropDown.css';
 import { Select } from 'antd';
 
-const SelectDropDown = ({ options = [], onChange, onSearch, placeholder = "Select", className = '' }) => {
+const SelectDropDown = ({ options = [], value, onChange, onSearch, placeholder = "Select", className = '' }) => {
     return (
         <Select
             showSearch
             placeholder={placeholder}
             optionFilterProp="label"
+            value={value}
             onChange={onChange}
             onSearch={onSearch}
             options={options}
