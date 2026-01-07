83ff158da1ab496c7c4b715fea80d90e54040fb6
Shoaib Farooka <shoaibfarooka@gmail.com>
2026-01-07 23:44:02 +0500

    error handler fixed

--- server/middleware/errorHandlerMiddleware.js
+++ server/middleware/errorHandlerMiddleware.js
@@ -1,7 +1,7 @@
 const errorHandlerMiddleware = (error, req, res, next) => {
     const code = error.code !== undefined ? Number(error.code) : undefined;
 
-    if (typeof code === 'number' && !isNaN(code) && error.message) {
+    if (typeof code === 'number' && !isNaN(code) && code >= 100 && code <= 599 && error.message) {
         return res.status(code).json({ error: error.message });
     } else {
         console.log(error);
